94d1f5b53861c60b001aad05eb1ca20a537a74e9
BigHeroHJ <lemonholl@LemondeiMac.local>
2018-01-08 13:15:23 +0800

    创建GLview

---
+++ OpenGLESDEMO/Learn1/learn1_view.h
@@ -0,0 +1,13 @@
+//
+//  learn1_view.h
+//  OpenGLESDEMO
+//
+//  Created by Lemon HOLL on 2018/1/8.
+//  Copyright © 2018年 Lemon HOLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface learn1_view : UIView
+
+@end
---
+++ OpenGLESDEMO/Learn1/learn1_view.m
@@ -0,0 +1,21 @@
+//
+//  learn1_view.m
+//  OpenGLESDEMO
+//
+//  Created by Lemon HOLL on 2018/1/8.
+//  Copyright © 2018年 Lemon HOLL. All rights reserved.
+//
+
+#import "learn1_view.h"
+
+@implementation learn1_view
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
