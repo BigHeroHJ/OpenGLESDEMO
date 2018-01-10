b0c2c55ba810006b5bde5006ff906e013b8c5b63
BigHeroHJ <lemonholl@LemondeiMac.local>
2018-01-10 13:34:59 +0800

    GLKVIew 使用练习

---
+++ OpenGLESDEMO/OpenGL ES_view2/OpenGlESView2.h
@@ -0,0 +1,15 @@
+//
+//  OpenGlESView2.h
+//  OpenGLESDEMO
+//
+//  Created by Lemon HOLL on 2018/1/10.
+//  Copyright © 2018年 Lemon HOLL. All rights reserved.
+// http://blog.csdn.net/icetime17/article/details/50451752
+// https://www.jianshu.com/p/ee597b2bd399
+// https://learnopengl-cn.github.io/01%20Getting%20started/01%20OpenGL/
+
+#import <UIKit/UIKit.h>
+
+@interface OpenGlESView2 : UIView
+
+@end
---
+++ OpenGLESDEMO/OpenGL ES_view2/OpenGlESView2.m
@@ -0,0 +1,28 @@
+//
+//  OpenGlESView2.m
+//  OpenGLESDEMO
+//
+//  Created by Lemon HOLL on 2018/1/10.
+//  Copyright © 2018年 Lemon HOLL. All rights reserved.
+//
+
+#import "OpenGlESView2.h"
+#import <OpenGLES/ES2/gl.h>
+#import <GLKit/GLKit.h>
+
+@implementation OpenGlESView2
+
+
++ (Class)class
+{
+    return [GLKView class];
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
